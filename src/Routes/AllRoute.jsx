f694ba766cf976b46164aa4e704d3e42e921404b
tawhidjony <tawhid.jony.jhm@gmail.com>
2021-06-19 02:14:23 +0600

    login

--- src/Routes/AllRoute.jsx
+++ src/Routes/AllRoute.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import {Switch, Route} from 'react-router-dom'
+import LoginScreen from '../Pages/Auth/Login/LoginScreen'
 import DashboardScreen from '../Pages/Dashboard/DashboardScreen'
 const AllRoute = () => {
     return (
         <Switch>
-            <Route exact path="/" component={DashboardScreen} />
+            <Route exact path="/" component={LoginScreen} />
+            <Route exact path="/dashboard" component={DashboardScreen} />
         </Switch>
     )
 }
