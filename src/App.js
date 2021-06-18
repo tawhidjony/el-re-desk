a3f395d1069b61c59e84f73a26594aacda2fffb6
tawhidjony <tawhid.jony.jhm@gmail.com>
2021-06-19 02:22:33 +0600

    add hash route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 import AllRoute from './Routes/AllRoute'
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <AllRoute />
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
